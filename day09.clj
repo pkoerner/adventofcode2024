a0de13be9e6b8662582038abdd6990393b3e852f
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2024-12-09 20:16:49 +0100

    day 09 part 2

--- day09.clj
+++ day09.clj
@@ -1,5 +1,5 @@
 (require 'clojure.string)
-(def input (map (into {} (map vector "0123456789" (range 10))) (clojure.string/trim (slurp "input09"))))
+(def input (mapv (into {} (map vector "0123456789" (range 10))) (clojure.string/trim (slurp "input09"))))
 
 (def disk (vec (repeat (reduce + input) 0)))
 
@@ -38,13 +38,54 @@
 
 (println "Part 1:" (checksum (defragment (restore-disk))))
 
-(defn create-freemap [disk]
-  (loop [disk disk
+;; this is far from optimal and has loads of potential for optimisation
+
+(defn get-count [id]
+  (get input (* 2 id)))
+
+
+(defn create-freelist [input]
+  (loop [disk input
          free false
          idx 0
-         freemap {}]
-    (cond (empty? disk) freemap
-          (not free) (recur (rest disk) (not free) (+ idx (first disk)) freemap)
-          :else (recur (rest disk) (not free) (+ idx (first disk)) (update freemap (first disk) (fn [v] (if v (conj v idx) [idx])))))))
+         freelist []]
+    (cond (empty? disk) freelist
+          (not free) (recur (rest disk) (not free) (+ idx (first disk)) freelist)
+          :else (recur (rest disk) (not free) (+ idx (first disk)) (conj freelist [(first disk) idx])))))
 
-(create-freemap input)
+#_ (create-freelist input)
+
+(defn first-free [freelist width]
+  (let [[prefix [[hitwidth hitpos] & tail]] (split-with #(not (>= (first %) width)) freelist)]
+    (if hitpos
+      [hitpos (doall (concat prefix [[(- hitwidth width) (+ hitpos width)]] tail))]
+      [nil prefix])))
+
+#_ (first-free (create-freelist input) 3)
+
+(defn move-block [disk rightend width leftfree]
+  (loop [disk disk
+         n width
+         rightend rightend
+         leftfree leftfree]
+    (if (zero? n)
+      disk
+      (recur (assoc disk rightend \. leftfree (get disk rightend)) 
+             (dec n)
+             (dec rightend)
+             (inc leftfree)))))
+
+(defn defragment2 [disk]
+  (loop [disk disk
+         freelist (create-freelist input)
+         right (dec (count disk))]
+    (cond (or (<= right 0)) disk
+          (= \. (get disk right)) (recur disk freelist (dec right))
+          :else (let [id (get disk right)
+                      width (get-count id)
+                      [hitpos freelist'] (first-free freelist width)]
+                  (if (< hitpos (- right width))
+                    (recur (move-block disk right width hitpos) freelist' (- right width))
+                    (recur disk freelist (- right width)))))))
+
+(println "Part 2:" (checksum (defragment2 (restore-disk))))
